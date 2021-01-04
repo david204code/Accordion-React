d27b61d9d63b52e9c722412fc83b7aa5afcd22c0
David Wu <david204code@gmail.com>
2021-01-04 16:56:59 +0000

    Project complete, setup state value, conditioning rendering and ternary operator

--- src/Question.js
+++ src/Question.js
@@ -2,17 +2,23 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const Question = ({title, info}) => {
+  // state value
+  const [showInfo, setShowInfo] = useState(false);
 
   return (
     <>
       <article className ="question">
         <header>
           <h4>{title}</h4>
-          <button className ="btn">
-            btn
+          <button className ="btn" onClick ={() => setShowInfo(!showInfo)}>
+            {/* ternary operator if showInfo is true then Minus else Plus*/}
+            {showInfo? <AiOutlineMinus /> : <AiOutlinePlus />}
           </button>
         </header>
-        <p className ="">{info}</p>
+        {
+          // if showInfo is true then display the paragraph
+          showInfo && <p className ="">{info}</p>
+        }
       </article>
     </>
   );
